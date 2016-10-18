07151fb02815dba8683fdc12d48c4395e54b04f3
amorrisgips <aamorris@gips.org>
2016-10-18 09:22:11 -0500

    Can not find out how to read from a file

--- app/src/main/java/com/example/sierra/basicrpg/MapLocation.java
+++ app/src/main/java/com/example/sierra/basicrpg/MapLocation.java
@@ -1,7 +1,18 @@
 package com.example.sierra.basicrpg;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.R.id.message;
 
 public class MapLocation extends AppCompatActivity {
 
@@ -10,4 +21,25 @@ public class MapLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_location);
     }
+
+    public void saveMess(View view) throws IOException {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message1 = editText.getText().toString();
+        FileOutputStream fos = openFileOutput("RPG_saveData", Context.MODE_PRIVATE);
+        fos.write(message1.getBytes());
+        fos.close();
+    }
+
+    public void dispMess(View view) throws FileNotFoundException {
+        FileOutputStream fos = openFileOutput("RPG_saveData", Context.MODE_PRIVATE);
+        Byte b[] = new Byte[10];
+        String message2 = fos.read(b);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message2);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_map_location);
+        layout.addView(textView);
+    }
+
 }
